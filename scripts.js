db3157729dac65eb867b7c62b5fb3f20f9188654
Oliver Kesi <oliverkesi@desktop-a5pe44r.kea.local>
2019-05-07 15:38:06 +0200

    Menu virker

---
+++ scripts.js
@@ -0,0 +1,22 @@
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+
+    document.querySelector("#menuknap").addEventListener("click", toggleMenu);
+
+}
+
+
+function toggleMenu() {
+    console.log("toggleMenu");
+    document.querySelector("#menu").classList.toggle("hidden");
+
+    let erSkjult = document.querySelector("#menu").classList.contains("hidden");
+
+    if (erSkjult == true) {
+        document.querySelector("#menuknap").textContent=("#burger");
+    } else {
+        document.querySelector("#menuknap").textContent = "X";
+    }
+}
