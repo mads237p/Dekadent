4a26efbf7e78dfab37b15aeed7305a8fa196cc7e
Oliver Kesi <oliverkesi@Olivers-MacBook-Pro-2.local>
2019-05-08 09:41:28 +0200

    burger menu skal fikses, men layout er fint.

--- scripts.js
+++ scripts.js
@@ -15,7 +15,7 @@ function toggleMenu() {
     let erSkjult = document.querySelector("#menu").classList.contains("hidden");
 
     if (erSkjult == true) {
-        document.querySelector("#menuknap").textContent=("#burger");
+        document.querySelector("#menuknap").textContent = ""
     } else {
         document.querySelector("#menuknap").textContent = "X";
     }
